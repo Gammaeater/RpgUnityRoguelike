20b134b7513b5b330a72115b45832bce77a25165
Livio <hipzap@gmail.com>
2020-01-21 01:07:11 +0100

    Implement Sound Loop
    Implamant Sound Loop

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,20 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void Awake()
-    {
-        //Time.timeScale = 0f;
-    }
+   
+
+    
     // Start is called before the first frame update
-  public void PlayGame ()
+    public void PlayGame()
     {
         SceneManager.LoadScene("Level1");
-        Time.timeScale = 1;
 
+        Time.timeScale = 1;
+        
     }
     public void QuitGame()
     {
