2f47e506e66486a05ae489a555de9bac10eb7b6f
Livio <hipzap@gmail.com>
2020-01-21 23:11:43 +0100

    Bug Fixin Cleanin Code
    Bug Fixin Cleanin Code

--- Assets/Scripts/GenericHealth.cs
+++ Assets/Scripts/GenericHealth.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class GenericHealth : MonoBehaviour
 {
 
-    public FloatValue maxHealth;
-    // Start is called before the first frame update
-    void Start()
-    {
+    //public FloatValue maxHealth;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
         
-    }
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
+    //// Update is called once per frame
+    //void Update()
+    //{
         
-    }
+    //}
 }
