eecdd29c893cc7c2427c5ef852bc49db25456e4c
Livio <hipzap@gmail.com>
2020-01-24 02:07:13 +0100

    Lev2Workin
    Level 2 fixing

---
+++ Assets/ColDetect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColDetect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D _other)
+    {
+
+        if (_other.gameObject.tag == "PlayerII2lev")
+        {
+
+            Debug.Log("It Collision detected?");
+                
+
+        }
+    }
+}
