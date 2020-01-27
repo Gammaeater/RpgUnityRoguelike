3329610a2a51db38e6312db9e6dfc8ec7729527a
Livio <hipzap@gmail.com>
2020-01-27 16:59:00 +0100

    Level 3 WiP
    Creating Boss Fight

--- Assets/MinotourBoss.cs
+++ Assets/MinotourBoss.cs
@@ -1,10 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MinotourBoss : MonoBehaviour
 {
     public Vector2 movment;
+    public Vector2 playermovment;
     public Transform _target;
     public float chaseRaidus;
     public float attackRadius;
@@ -13,13 +12,14 @@ public class MinotourBoss : MonoBehaviour
     public Animator anim;
     public PlayerIIMovment _playerTarget;
     public float moveSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         _target = GameObject.FindWithTag("PlayerII").transform;
 
-        anim = GetComponent<Animator>();
+
         _playerTarget = GameObject.FindWithTag("PlayerII").GetComponent("PlayerIIMovment") as PlayerIIMovment;
 
     }
@@ -27,38 +27,39 @@ public class MinotourBoss : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        movment.x = transform.position.normalized.x;
-        movment.y = transform.position.normalized.y;
-
-        movment = transform.position;
-       
-        anim.SetFloat("Horizontal", movment.normalized.x);
-        anim.SetFloat("Vertical", movment.y);
-        anim.SetFloat("Speed", moveSpeed);
-        CheckDistance();
-        float distance = Vector3.Distance(_target.transform.position, transform.position);
+        movment.x = transform.position.x;
+        playermovment.x = _target.position.x;
 
         
 
-        if (distance < 2.5f )
+        //anim.SetFloat("Horizontal", movment.normalized.x);
+        //anim.SetFloat("Vertical", movment.y);
+
+        CheckDistance();
+        float distance = Vector3.Distance(_target.transform.position, transform.position);
+
+
+
+        if (distance < 10.5f)
 
 
         {
+            Debug.Log("Distane checkinnnnnnnnnnnnnnnnnnnnnnng");
 
-
+           //anim.SetBool("isMoving", false);
 
             if (Time.time > timeSinceLastShot + TimeBetweenShots)
             {
 
                 //StartCoroutine(DmgSpawn());
-               // Attack(3f);
-
-
+                Attack(2f);
+                Debug.Log("Its works?");
+                //Tount();
 
 
                 timeSinceLastShot = Time.time;
 
-            
+
 
 
             }
@@ -74,6 +75,11 @@ public class MinotourBoss : MonoBehaviour
 
 
         }
+        else
+        {
+            anim.SetBool("isMoving", true);
+        }
+
 
 
 
@@ -89,28 +95,43 @@ public class MinotourBoss : MonoBehaviour
         if (Vector2.Distance(_target.position, transform.position) <= chaseRaidus
             && Vector2.Distance(_target.position, transform.position) > attackRadius)
         {
+
             transform.position = Vector2.MoveTowards(transform.position, _target.position, moveSpeed * Time.deltaTime);
-           
+            anim.SetBool("isMoving", true);
+
+
+            if (movment.x < playermovment.x)
+            {
+                anim.SetBool("RunLeft", false);
+                anim.SetBool("RunRight", true);
+
+
+            }
+            else
+            {
+                anim.SetBool("RunRight", false);
+                anim.SetBool("RunLeft", true);
 
 
 
+
+
+
+            }
+           // anim.SetBool("isMoving", false);
 
 
 
         }
-
-
-
-
     }
 
     public void Attack(float damage)
     {
 
         //randomBonusHit = (float)Random.Range(1, 5);
-       // batfullAttack = baseAtack + randomBonusHit;
+        // batfullAttack = baseAtack + randomBonusHit;
 
-        _playerTarget.playerHealtShystem.Damage(damage);
+        //_playerTarget.playerHealtShystem.Damage(damage);
 
 
 
@@ -127,7 +148,7 @@ public class MinotourBoss : MonoBehaviour
         //randomBonusHit = (float)Random.Range(1, 5);
         // batfullAttack = baseAtack + randomBonusHit;
 
-        _playerTarget.playerHealtShystem.Damage(damage);
+        // _playerTarget.playerHealtShystem.Damage(damage);
 
 
 
@@ -137,20 +158,39 @@ public class MinotourBoss : MonoBehaviour
 
     }
 
-    public void Attack3(float damage)
+    public void Tount()
     {
-
-        //randomBonusHit = (float)Random.Range(1, 5);
-        // batfullAttack = baseAtack + randomBonusHit;
-
-        _playerTarget.playerHealtShystem.Damage(damage);
-
-
-
-
-
-
+        anim.SetBool("IsCharging", true);
+        if (movment.x < playermovment.x)
+        {
+            //left
+            anim.SetBool("ChargeLeft", true);
+        }
+        else
+        {
+            anim.SetBool("ChargeRight", true);
+        }
 
     }
+    public void CheckHoritontalPOosition()
+    {
+        if (movment.x < playermovment.x)
+        {
+            anim.SetBool("isMoving", true);
+            anim.SetBool("RunLeft", false);
+            anim.SetBool("RunRight", true);
 
+
+        }
+        else
+        {
+            anim.SetBool("RunRight", false);
+            anim.SetBool("RunLeft", true);
+
+
+        }
+    }
 }
+
+
+
